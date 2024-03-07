953c2e16ac7c1c45b03923d1d1e487319eaa7b62
fatamie19 <22ftt1507@student.pb.edu.bn>
2024-03-07 15:36:18 +0800

    Refactor tab layout in app.component.html and tabs.page.html

--- Places/src/app/tabs/tabs.page.ts
+++ Places/src/app/tabs/tabs.page.ts
@@ -2,19 +2,20 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { addIcons } from 'ionicons';
+import { triangle, ellipse, square } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
   templateUrl: './tabs.page.html',
   styleUrls: ['./tabs.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule],
 })
 export class TabsPage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({ triangle, ellipse, square });
   }
 
+  ngOnInit() {}
 }
