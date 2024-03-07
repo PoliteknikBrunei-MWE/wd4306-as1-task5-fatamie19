48c1d0781f3152367a4c29bf96bc63451c395fce
fatamie19 <22ftt1507@student.pb.edu.bn>
2024-03-07 15:07:53 +0800

    Update routes to redirect to the 'auth' page instead of 'home'

--- Places/src/app/app.routes.ts
+++ Places/src/app/app.routes.ts
@@ -2,16 +2,16 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    path: 'auth',
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
   },
   {
     path: '',
-    redirectTo: 'home',
+    redirectTo: 'auth',
     pathMatch: 'full',
   },
   {
     path: 'auth',
-    loadComponent: () => import('./auth/auth.page').then( m => m.AuthPage)
+    loadComponent: () => import('./auth/auth.page').then((m) => m.AuthPage),
   },
 ];
